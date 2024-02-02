50b82b5f8683e8954909bacd9a09f64317550c4c
Hristo <144589040+HristoMihovski@users.noreply.github.com>
2024-02-02 11:44:29 +0200

    Putting the whole project successfully

---
+++ SoftUniPB_Tests/SoftUniPB_Tests/ProcessingClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class ProcessingClass
+{
+    public static int Process()
+    {
+        string destination;
+        double ticketPrice = 0;
+        destination = Console.ReadLine();
+
+        while (destination != "End")
+        {
+            ticketPrice = Convert.ToDouble(Console.ReadLine());
+            double collectedMoney = 0;
+
+            while (collectedMoney < ticketPrice)
+            {
+                double earnedMoney;
+                earnedMoney = Convert.ToDouble(Console.ReadLine());
+                collectedMoney += earnedMoney;
+            }
+
+            if (collectedMoney >= ticketPrice)
+            {
+                Console.WriteLine($"Going to {destination}!");
+            }
+
+            destination = Console.ReadLine();
+        }
+
+        return 0;
+    }
+
+    static void Main(string[] args)
+    {
+        Process();
+    }
+}
+
---
+++ SoftUniPB_Tests/SoftUniPB_Tests/TestFile.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+public struct TestParam
+{
+    public int TestCaseNumber;
+    public string Input;
+    public string ExpectedOutput;
+}
+
+public class PbTest
+{
+    private const string TestScenarioSeparator = "===";
+
+    public static IEnumerable<TestParam> GenerateTestParams()
+    {
+        var paramsList = new List<TestParam>();
+        using (var inputFile = new StreamReader("../tests/testInputs.txt"))
+        using (var expectedOutputFile = new StreamReader("../tests/testExpectedResults.txt"))
+        {
+            int testCaseNumber = 0;
+            string line;
+            string input = string.Empty;
+            string expectedOutput = string.Empty;
+
+            while ((line = inputFile.ReadLine()) != null)
+            {
+                if (line == TestScenarioSeparator)
+                {
+                    string outputLine;
+                    while ((outputLine = expectedOutputFile.ReadLine()) != null && outputLine != TestScenarioSeparator)
+                    {
+                        expectedOutput += outputLine + "\n";
+                    }
+                    paramsList.Add(new TestParam { TestCaseNumber = testCaseNumber++, Input = input, ExpectedOutput = expectedOutput });
+                    input = string.Empty;
+                    expectedOutput = string.Empty;
+                }
+                else
+                {
+                    input += line + "\n";
+                }
+            }
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                string outputLine;
+                while ((outputLine = expectedOutputFile.ReadLine()) != null && outputLine != TestScenarioSeparator)
+                {
+                    expectedOutput += outputLine + "\n";
+                }
+                paramsList.Add(new TestParam { TestCaseNumber = testCaseNumber, Input = input, ExpectedOutput = expectedOutput });
+            }
+        }
+
+        return paramsList;
+    }
+
+    [Test]
+    [TestCaseSource(nameof(GenerateTestParams))]
+    public void Test(TestParam param)
+    {
+        using (var iss = new StringReader(param.Input))
+        using (var ossOutput = new StringWriter())
+        {
+            ProcessingClass.Process();
+            Assert.Equals(param.ExpectedOutput, ossOutput.ToString());
+        }
+    }
+}
