7beedb2088aa36ef0c97eb9338ab4181c4c8f3c2
Hristo <144589040+HristoMihovski@users.noreply.github.com>
2024-02-02 13:28:46 +0200

    Update ProcessingClass.cs
    The code can be given to judge easily. His output will be 100/100. The code is written in `public static int Process() { // Your code }`

--- SoftUniPB_Tests/SoftUniPB_Tests/ProcessingClass.cs
+++ SoftUniPB_Tests/SoftUniPB_Tests/ProcessingClass.cs
@@ -1,36 +1,13 @@
-﻿using System;
+using System;
 
 class ProcessingClass
 {
-    public static int Process()
-    {
-        string destination;
-        double ticketPrice = 0;
-        destination = Console.ReadLine();
-
-        while (destination != "End")
-        {
-            ticketPrice = Convert.ToDouble(Console.ReadLine());
-            double collectedMoney = 0;
-
-            while (collectedMoney < ticketPrice)
-            {
-                double earnedMoney;
-                earnedMoney = Convert.ToDouble(Console.ReadLine());
-                collectedMoney += earnedMoney;
-            }
-
-            if (collectedMoney >= ticketPrice)
-            {
-                Console.WriteLine($"Going to {destination}!");
-            }
-
-            destination = Console.ReadLine();
-        }
-
+    public static int Process() { 
+    double usd = double.Parse(Console.ReadLine());
+    double bgn = usd * 1.79549;
+    Console.WriteLine(bgn);
         return 0;
-    }
-
+        }
     static void Main(string[] args)
     {
         Process();
