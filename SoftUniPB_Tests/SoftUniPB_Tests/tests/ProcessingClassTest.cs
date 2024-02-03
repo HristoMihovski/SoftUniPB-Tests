e8558c8917bbb0055dcedd1375506ef8165293ef
Hristo <144589040+HristoMihovski@users.noreply.github.com>
2024-02-03 18:06:44 +0200

    Update ProcessingClassTest.cs

--- SoftUniPB_Tests/SoftUniPB_Tests/tests/ProcessingClassTest.cs
+++ SoftUniPB_Tests/SoftUniPB_Tests/tests/ProcessingClassTest.cs
@@ -1,60 +1,80 @@
-using System;
-using System.IO;
-using System.Collections.Generic;
 using NUnit.Framework;
 
-[TestFixture]
-public class ProcessingClassTest
+namespace PBTests
 {
-    private const string TestScenarioSeparator = "===";
 
-    private static IEnumerable<TestParam> GenerateTestParams()
+    [TestFixture]
+    public class ProcessingClassTest
     {
-        var paramsList = new List<TestParam>();
-        var inputFile = new StreamReader("testInputs.txt");
-        var expectedOutputFile = new StreamReader("testExpectedResults.txt");
-        string line;
-        int testCaseNumber = 0;
-        string input = "";
-        string expectedOutput = "";
-        while ((line = inputFile.ReadLine()) != null)
+        private const string TestScenarioSeparator = "===";
+
+        private static IEnumerable<TestParam> GenerateTestParams()
         {
-            if (line == TestScenarioSeparator)
+            var paramsList = new List<TestParam>();
+            // Hardcoded full path to the test files
+            var inputFile = new StreamReader(@"C:\Users\hrist\Downloads\VR\PBTests\PBTests\testInputs.txt");
+            var expectedOutputFile = new StreamReader(@"C:\Users\hrist\Downloads\VR\PBTests\PBTests\testExpectedResults.txt");
+
+            string line;
+            int testCaseNumber = 0;
+            string input = "";
+            string expectedOutput = "";
+            while ((line = inputFile.ReadLine()) != null)
+            {
+                if (line == TestScenarioSeparator)
+                {
+                    string outputLine;
+                    while ((outputLine = expectedOutputFile.ReadLine()) != null && outputLine != TestScenarioSeparator)
+                    {
+                        // Replace carriage return and newline characters with just newline characters
+                        expectedOutput += outputLine.Replace("\r\n", "\n") + "\n";
+                    }
+                    paramsList.Add(new TestParam { TestCaseNumber = testCaseNumber++, Input = input, ExpectedOutput = expectedOutput });
+                    input = "";
+                    expectedOutput = "";
+                }
+                else
+                {
+                    input += line + "\n";
+                }
+            }
+            if (!string.IsNullOrEmpty(input))
             {
                 string outputLine;
                 while ((outputLine = expectedOutputFile.ReadLine()) != null && outputLine != TestScenarioSeparator)
                 {
-                    expectedOutput += outputLine + "\n";
+                    // Replace carriage return and newline characters with just newline characters
+                    expectedOutput += outputLine.Replace("\r\n", "\n") + "\n";
                 }
-                paramsList.Add(new TestParam { TestCaseNumber = testCaseNumber++, Input = input, ExpectedOutput = expectedOutput });
-                input = "";
-                expectedOutput = "";
+                paramsList.Add(new TestParam { TestCaseNumber = testCaseNumber, Input = input, ExpectedOutput = expectedOutput });
+            }
+            return paramsList;
+        }
+
+
+        [TestCaseSource("GenerateTestParams")]
+        public void Test(TestParam param)
+        {
+            // Ensure param.Input and param.ExpectedOutput are not null before using them
+            if (param.Input != null && param.ExpectedOutput != null)
+            {
+                using (var input = new StringReader(param.Input))
+                using (var output = new StringWriter())
+                {
+                    ProcessingClass.Process(input, output);
+
+                    // Remove carriage return and newline characters for comparison
+                    var expected = param.ExpectedOutput.Replace("\r", "").Replace("\n", "");
+                    var actual = output.ToString().Replace("\r", "").Replace("\n", "");
+
+                    Assert.AreEqual(expected, actual, "Test failed for TestCaseNumber: " + param.TestCaseNumber);
+                }
             }
             else
             {
-                input += line + "\n";
+                Assert.Fail("Input or ExpectedOutput is null.");
             }
         }
-        if (!string.IsNullOrEmpty(input))
-        {
-            string outputLine;
-            while ((outputLine = expectedOutputFile.ReadLine()) != null && outputLine != TestScenarioSeparator)
-            {
-                expectedOutput += outputLine + "\n";
-            }
-            paramsList.Add(new TestParam { TestCaseNumber = testCaseNumber, Input = input, ExpectedOutput = expectedOutput });
-        }
-        return paramsList;
     }
 
-    [TestCaseSource("GenerateTestParams")]
-    public void Test(TestParam param)
-    {
-        using (var input = new StringReader(param.Input))
-        using (var output = new StringWriter())
-        {
-            ProcessingClass.Process(input, output);
-            Assert.AreEqual(param.ExpectedOutput, output.ToString());
-        }
     }
-}
