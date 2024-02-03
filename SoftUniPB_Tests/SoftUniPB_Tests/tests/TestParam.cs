c095bd87cc2384c2633e28eba0f9c86878f999fa
Hristo <144589040+HristoMihovski@users.noreply.github.com>
2024-02-03 18:07:47 +0200

    Create TestParam.cs

---
+++ SoftUniPB_Tests/SoftUniPB_Tests/tests/TestParam.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBTests
+{
+    public class TestParam
+    {
+        public int TestCaseNumber { get; set; }
+        public string? Input { get; set; }
+        public string? ExpectedOutput { get; set; }
+    }
+}
